b417d2c8487c9550b6c716036120e3100eaa4b89
Chilayyy <le172@cougars.csusm.edu>
2021-01-15 06:10:34 +0000

    lesson 2 vector to git bash

--- lesson_2/intro_vector.cpp
+++ lesson_2/intro_vector.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main()
 {
+	vector<int> vec;
+	cout << "vector's size is " << vec.size() << endl;
 	
+	for(int i = 1; i <= 5; i++)
+	{
+		vec.push_back(i);
+	}
+
+	for(int i = 0; i < vec.size(); i++)
+	{
+		cout <<  vec[i] << endl;
+	}	
 }
