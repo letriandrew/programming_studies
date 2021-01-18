801cdff1b16cd751f92eff4146d8fdb9fd7d70c1
Andrew Le <andrew@vmi434461.contaboserver.net>
2021-01-18 01:14:19 -0600

    new assignment

---
+++ lesson_2/stringtovector.cpp
@@ -0,0 +1,11 @@
+/* get user input (sentence) (getline lol) transfer every single char into vector. with vector, capitalize every char and then print it backwards
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+}
