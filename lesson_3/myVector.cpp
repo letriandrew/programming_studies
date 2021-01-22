571063f55d4d3f58f3158435fe6264c82b990253
Andrew Le <andrew@vmi434461.contaboserver.net>
2021-01-21 22:55:44 -0600

    assignment

--- lesson_3/myVector.cpp
+++ lesson_3/myVector.cpp
@@ -23,3 +23,46 @@ void printVector(vector<int> vec)
 	
 	cout << endl;
 }
+
+vector<int> reverseVector(vector<int> vec)
+{
+	vector<int> revvec = vec;
+	int xd = 0;
+	
+	for(int i = vec.size()-1; i >= 0; i--)
+	{
+		revvec[xd] = vec[i];
+		xd++;
+	}
+	
+	vec = revvec;
+}
+
+void emptyVector(vector<int>& vec)
+{
+	//function to clear vec
+	vec.clear();
+}
+
+vector<int> multVector(vector<int> vec, int mult)
+{
+	//for loop to go through each vector interval and multiply it back int mult
+	for(int i = 0; i < vec.size(); i++)
+	{
+		vec[i] = vec[i] * mult;
+	}
+}
+
+bool isEmptyVector(vector<int> vec)
+{
+	//function checks if vec is empty and cout result
+	if(vec.empty())
+	{
+		cout << "The vector is empty. " << endl;
+	}
+
+	else
+	{
+		cout  << "The vector is not empty. " << endl;
+	}
+}
