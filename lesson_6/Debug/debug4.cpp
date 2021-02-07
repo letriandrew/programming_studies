5f3a14e37f83712408787c214789baca71d77ace
Chilayy <le172@cougars.csusm.edu>
2021-02-06 22:50:28 -0800

    lol 3

---
+++ lesson_6/Debug/debug4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+	string words = "hello"; 
+    
+	if(words == "hello")
+    {
+        cout << "hello there!" << endl;
+    }
+
+    else if(words == "goodbye")
+	{
+    	cout << "Goodbye!" << endl;
+    }
+    
+	else
+	{
+		cout << "what are you saying" << endl;
+	}
+}
